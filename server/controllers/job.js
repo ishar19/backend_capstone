b1b70437923ae4eb0514425159f7a70126fa49ad
ishar19 <isharjain19@gmail.com>
2024-05-20 23:02:23 +0530

    feat: added get and edit jobs apis

--- server/controllers/job.js
+++ server/controllers/job.js
@@ -47,5 +47,57 @@ const createJob = async (req, res, next) => {
     next(err);
   }
 };
+const getAllJobs = async (req, res, next) => {
+  try {
+    const jobs = await Job.find()
+      .select(["title", "skills", "companyName"]) // select only these fields
+      .sort({ createdAt: -1 }); // -1 for descending order
+    res.status(200).send(jobs);
+  } catch (err) {
+    next(err);
+  }
+};
 
-module.exports = { createJob };
+const getJobById = async (req, res, next) => {
+  try {
+    const { jobnumber } = req.params;
+    const job = await Job.findById(jobnumber);
+    // if job is not found in the database it will give 500 because Job.findById() will throw an error instead of returning null
+    if (!job) {
+      return res.status(404).send("Job not found");
+    }
+    res.status(200).send(job);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateJob = async (req, res, next) => {
+  try {
+    const { jobnumber } = req.params;
+    const job = await Job.findById(jobnumber);
+    const skillsArray = req.body.skill
+      ? req.body.skills.split(",").map((skill) => skill.trim())
+      : null;
+    const updatedJob = await Job.findByIdAndUpdate(
+      jobnumber,
+      {
+        title: req.body.title || job.title,
+        companyName: req.body.companyName || job.companyName,
+        location: req.body.location || job.location,
+        salary: req.body.salary || job.salary,
+        description: req.body.description || job.description,
+        locationType: req.body.locationType || job.locationType,
+        jobType: req.body.jobType || job.jobType,
+        skills: skillsArray || job.skills,
+        updatedAt: new Date(),
+        createdAt: job.createdAt,
+      },
+      { new: true }
+    );
+    res.status(200).send(updatedJob);
+  } catch (err) {
+    next(err);
+  }
+};
+module.exports = { createJob, getAllJobs, getJobById, updateJob };
--- server/routes/job.js
+++ server/routes/job.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { createJob } = require("../controllers/job");
+const {
+  createJob,
+  getAllJobs,
+  getJobById,
+  updateJob,
+} = require("../controllers/job");
 const { verifyAuth } = require("../middleware/verifyAuth");
 router.get("/", (req, res) => {
   res.status(200).send("Job Route!");
 });
 router.post("/create", verifyAuth, createJob);
+router.get("/all", getAllJobs);
+router.get("/view/:jobnumber", getJobById); // job/123576
+router.patch("/update/:jobnumber", verifyAuth, updateJob);
 module.exports = router;
